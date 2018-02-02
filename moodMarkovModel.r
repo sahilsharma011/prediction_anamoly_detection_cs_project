64fb4591316796b976ab6ed8ca2197f8f5be587b
Sahil Sharma <sahilsharma011@users.noreply.github.com>
2018-02-03 02:39:37 +0530

    Made data import dynamic
    And also using correct labels

--- moodMarkovModel.r
+++ moodMarkovModel.r
@@ -1,5 +1,8 @@
 library("markovchain")
-xChar<-as.character(c(1,5,1,5,1,3,1,5,3,3,5,6,6,1,4,3,1,1,1,1,3,1,6,2,2,1,1,5,1,1,1,2,2,1,1,5,4,4,3,1,1,1,1,1,1,3,5,2,1,4,1,4,1,1,1,1,1,2,5,1,1,2,1,2,4,2,2,1,3,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,1,3,1,3,3,1,1,1,3,2,2,5,4,1,1,3,1,1,1,3,3,1,1,1,3,1,4,1,2,1,1,3,1,6,4,1,1,1,2,1,1,2,2,2,1,4,2,2,2,1,2,2,2,1,3,1,2,6,2,2,1,1,1,2,1,2,2,2,1,1))
+states<-c("Normal","Pressure","Happy","Sadness","Angry","Fear")
+#Reads all csv file in storage and gets mood data from there
+data<-foreach(elem=list.files("R",pattern = ".+(\\.csv)$"),.combine = 'c') %do% data.matrix(read.csv(paste('R\\',elem,sep=""),sep = ',',header = F))
+xChar<-foreach(elem=data, .combine='c') %do% states[elem]
 mcX<-markovchainFit(xChar)$estimate                       #creates markov chain from transition matrix
 plot(mcX)                                                 #Plots the markov chain visually
-myMood<-rmarkovchain(n = 365, object = mcX, t0 = 1)       #Stimulates my mood for the next year
+myMood<-rmarkovchain(n = 365, object = mcX, t0 = states[1])       #Stimulates my mood for the next year
